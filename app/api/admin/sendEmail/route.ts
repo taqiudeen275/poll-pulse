0bad6c30181affbad6334dd646300205dabefaca
taqiudeen275 <55208368+taqiudeen275@users.noreply.github.com>
2024-05-02 16:21:54 +0000

    sms only

--- app/api/admin/sendEmail/route.ts
+++ app/api/admin/sendEmail/route.ts
@@ -8,51 +8,12 @@ interface PermitProp {
     phone_number: string;
 }
 
-export async function POST(request: Request){
+export default async function POST(request: Request){
   try {
       const {permit} = await request.json();
-      // const { SMTP_EMAIL, SMTP_PASSWORD, SITE_URL, SMS_API_KEY } = process.env;
-  
-  const transport = nodemailer.createTransport({
-    service: "gmail",
-    host: "smtp.gmail.com",
-    secure: true,
-    port: 465,
-    auth: {
-      user: process.env.SMTP_EMAIL,
-      pass: process.env.SMTP_PASSWORD,
-    },
-  });
-
-  await new Promise((resolve, reject) => {
-    // verify connection configuration
-    transport.verify(function (error, success) {
-        if (error) {
-            console.log(error);
-            reject(error);
-        } else {
-            console.log("Server is ready to take our messages");
-            resolve(success);
-        }
-    });
-});
-  
+ 
 
     permit.forEach(async (permit: PermitProp) => {
-      await new Promise((resolve, reject) => {
-      transport.sendMail({
-          from: process.env.SMTP_EMAIL,
-          to: permit.email,
-          subject: "Vote Permit Code",
-          html: `
-          <h3>Hello ${permit.name}</h3>
-          <p>Your Vote Permit Code is <h3>${permit.permit}</h3></p>
-          <p>Use this code to vote, it is valid for 24hours</p>
-          <a href="https://www.udsmsaelection24.com/voter/permit">Click here to vote </a>
-          `,
-        });
-        
-      });
 
       await new Promise((resolve, reject) => {
         const message =  `
